5d406c116e234f55c9a287bd43a9b8013610342c
= <=>
2023-09-26 21:49:13 -0300

    atualiza assetlinks

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return redirect()->route('dashboard');
 });
 
+Route::get('/payment', function () {
+    return response()->json();
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
